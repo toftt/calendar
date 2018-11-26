0081b85eb9ba17cf8916c162305e281a4442aa3f
Joachim Toft <c.joachim.toft@gmail.com>
2018-11-26 20:53:52 +0100

    save token in localStorage

--- src/api/auth.js
+++ src/api/auth.js
@@ -13,13 +13,13 @@ const authorize = () => {
   window.location.hash = '';
 
   // Set token
-  let _token = hash.access_token;
+  let _token = hash.access_token || getLocalStorageToken();
 
   const authEndpoint = 'https://accounts.spotify.com/authorize';
 
   // Replace with your app's client ID, redirect URI and desired scopes
   const clientId = 'de8a7d941d164ea4854a21ebaca90c71';
-  const redirectUri = 'http://c836f32c.eu.ngrok.io';
+  const redirectUri = 'http://09de258c.eu.ngrok.io';
   const scopes = [
     'user-top-read',
     'user-modify-playback-state'
@@ -35,7 +35,29 @@ const authorize = () => {
     window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true&state=${state}`;
   }
 
-  return ({ token: _token, hashState: hash.state});
+  // case 1: user is in view mode and has been redirected from spotify -- tracks are in hash.state (the url hash)
+  // case 2: user is in view mode and has already authorized -- tracks are in tracks (params)
+  // case 3: user is in edit mode and has already authorized -- there are no tracks
+  const hashState = hash.state || tracks || 'null';
+
+  saveTokenInfo(_token, hash.expires_in);
+  return ({ token: _token, hashState});
 }
+
+const getLocalStorageToken = () => {
+  const token = localStorage.getItem('token');
+  const expirationTime = localStorage.getItem('expirationTime');
+  const now = new Date();
+
+  if (!token || now.getTime() > expirationTime) return undefined;
+  return token;
+};
+
+const saveTokenInfo = (token, expiresIn) => {
+  localStorage.setItem('token', token);
+  localStorage.setItem('expirationTime', (new Date()).getTime + expiresIn);
+}
+
+
 
 export default authorize;
