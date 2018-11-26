fa14273702a1c1accea3160cd987f3939c4eeff8
Joachim Toft <c.joachim.toft@gmail.com>
2018-11-26 21:37:16 +0100

    Updates

---
+++ precache-manifest.6df4e4cc140d63798eef4c7083df4307.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8c9fae71e5d28e44ecc",
+    "url": "/calendar/static/js/runtime~main.d8c9fae7.js"
+  },
+  {
+    "revision": "bb806ea739caeeca21c9",
+    "url": "/calendar/static/js/main.bb806ea7.chunk.js"
+  },
+  {
+    "revision": "ccdf379248f8a467d8b9",
+    "url": "/calendar/static/js/1.ccdf3792.chunk.js"
+  },
+  {
+    "revision": "bb806ea739caeeca21c9",
+    "url": "/calendar/static/css/main.b91b8c9d.chunk.css"
+  },
+  {
+    "revision": "20d282df3781d2632216db23699dd2fb",
+    "url": "/calendar/index.html"
+  }
+];
